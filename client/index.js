b50352fe784a162c699b1fd8e906cb718e2bb923
unknown <you@example.com>
2020-08-23 12:54:54 +0300

    return data retrieved from the database to the table

--- client/index.js
+++ client/index.js
@@ -41,6 +41,18 @@ const table = document.querySelector('table tbody');
 
 if(data.length === 0) {
     table.innerHTML = "<tr><td class='no-data' colspan='5'>No Data</td></tr>"
+    return;
 }
+let tableHtml = "";
 
+data.forEach(function ({id, name, date_added}) {
+    tableHtml += "<tr>";
+    tableHtml += `<td>${id}</td>`;
+    tableHtml += `<td>${name}</td>`;    
+    tableHtml += `<td>${date_added}</td>`;  
+    tableHtml += `<td><button class="delete-row-btn" data-id=${id}>Delete</td>`;  
+    tableHtml += `<td><button class="edit-row-btn" data-id=${id}>Edit</td>`;  
+    tableHtml += "</tr>";
+})
+table.innerHTML = tableHtml;
 }
